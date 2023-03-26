68d78eb7914ce25ee5de120b98426372f417c32e
Bismuth10K <bismuthdev@gmail.com>
2023-03-26 20:11:48 +0200

    Finished command handling

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const client = new Client({intents: [GatewayIntentBits.Guild]});
 // Collection is used to store and efficiently retrieve commands for execution.
 client.commands = new Collection();
 
+// path.join() helps to construct a path to the commands directory
 const commandsPath = path.join(__dirname, 'commands');
+//  fs.readdirSync() method then reads the path to the directory and returns an array of all the file names it contains
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles){
@@ -26,10 +28,26 @@ for (const file of commandFiles){
 	}
 }
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = client.commands.get(interaction.commandName)
+
+	if (!command){
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try{
+		await command.execute(interaction);
+	} catch(error){
+		console.error(error);
+		if (interaction.replied || interaction.deferred){
+			await interaction.followUp({content: 'There was an error while executing this command!', ephemeral: true});
+		} else {
+			await interaction.reply({content: 'There was an error while executing this command!', ephemeral: true});
+		}
+	}
 });
 
 // Log in to Discord with your client's token
